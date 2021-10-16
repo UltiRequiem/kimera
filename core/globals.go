418cc2053a1ab46ff8b91bf30d745e03767d7a4a
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-10-16 13:21:59 -0500

    :sparkles: Update

---
+++ core/globals.go
@@ -0,0 +1,18 @@
+package core
+
+import (
+	"fmt"
+
+	"github.com/lithdew/quickjs"
+)
+
+func Globals(ctx *quickjs.Context, this quickjs.Value, args []quickjs.Value) quickjs.Value {
+
+	switch args[0].String() {
+
+	case "console":
+		fmt.Println(args[1].String())
+	}
+
+	return ctx.Null()
+}
--- core/root.go
+++ core/root.go
@@ -1,37 +1,40 @@
 package core
 
 import (
-	"fmt"
 	"os"
+        _ "embed"
 	"runtime"
 
 	"github.com/lithdew/quickjs"
 )
 
+//go:embed std/*
+var codeGlobals string
+
 func RunFile(fileToRun string) {
 
 	code, errorReadingFile := os.ReadFile(fileToRun)
 
-	if errorReadingFile != nil {
-		fmt.Println(errorReadingFile)
-                os.Exit(1)
-	}
+	CheckJSError(errorReadingFile)
 
 	// Ensure that always operates in the exact same thread
 	runtime.LockOSThread()
 
 	runtimejs := quickjs.NewRuntime()
 	defer runtimejs.Free()
-
 	context := runtimejs.NewContext()
 	defer context.Free()
 
+	globals := context.Globals()
+	globals.Set("__dispatch", context.Function(Globals))
+
+	k, errorInjectingGlobals := context.Eval(codeGlobals)
+	CheckJSError(errorInjectingGlobals)
+	defer k.Free()
+
 	result, error := context.EvalFile(string(code), "s")
 
-	if error != nil {
-		fmt.Printf("Error while running %s.\n", fileToRun)
-		fmt.Println(error)
-	}
+	CheckJSError(error)
 
 	result.Free()
 }
